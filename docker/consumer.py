ffc03b7299e784a836d9bb93094b5b1ecbfc5c79
secr <ch.liefrid@gmail.com>
2024-05-12 00:11:30 +0100

    add consumer

--- docker/consumer.py
+++ docker/consumer.py
@@ -1,52 +1,34 @@
-import json
 from kafka import KafkaConsumer
-import pandas as pd
-from pyspark.sql import SparkSession
-from pyspark.ml import PipelineModel
-from pymongo import MongoClient
-print("INFO : started Consumer")
-# Establish a connection to MongoDB
-client = MongoClient('mongodb', 27017)
-db = client['twitter_database']
-collection = db['twitter_collection']
-
-# Création de la session Spark
-spark = SparkSession.builder \
-    .appName("Twitter Sentiment Analysis") \
-    .getOrCreate()
-
-
-# Load the pre-trained model
-model = PipelineModel.load('pipeline_model/')
+import json
+import requests
 
 # Create a KafkaConsumer instance
 consumer = KafkaConsumer(
-    bootstrap_servers=['kafka:9092'],
+    bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=False
 )
 
 # Subscribe to a specific topic
 consumer.subscribe(topics=['twitter'])
+
+# Define the URL of the Flask API endpoint
+API_URL = 'http://localhost:5000/predict'
+
 for message in consumer:
-    print("INFO: Received message")
-    # Deserialize JSON message to a Python dictionary
-    message_dict = json.loads(message.value)
+    try:
+        # Deserialize JSON message to a Python dictionary
+        print(message.value.decode('utf-8'))
 
-    # Convert dictionary to a Pandas DataFrame
-    df = pd.DataFrame([message_dict])
+        # Send HTTP POST request to the Flask API endpoint with the message data
+        response = requests.post(API_URL, json=message.value.decode('utf-8'))
 
-    # Convert Pandas DataFrame to Spark DataFrame
-    spark_df = spark.createDataFrame(df)
 
-    # Transform the DataFrame using the model
-    transformed_data = model.transform(spark_df)
+        # Check if the request was successful
+        if response.status_code == 200:
+            print("INFO: Data processed and saved successfully")
+        else:
+            print("ERROR: Failed to process data:", response.text)
+    except Exception as e:
+        print("ERROR:", str(e))
 
-    # Convert Spark DataFrame to Pandas DataFrame
-    transformed_df = transformed_data.toPandas()
-
-    # Perform further processing with the transformed data
-    records = transformed_df.to_dict(orient='records')
-    for record in records:
-        print("INFO : inserting records")
-        collection.insert_one(record)
